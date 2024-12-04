b75743b817e8492e650164d8b3b65c2f2d1eb0db
Paudie Scanlon <104898494+PaudieScanlon@users.noreply.github.com>
2024-12-04 05:54:41 +0000

    Create Q16A.py

---
+++ Calculator & Turtle/Q16A.py
@@ -0,0 +1,12 @@
+#Question 16A
+#Name:
+
+num1 = 5
+num2 = 10
+
+choice = str(input("Do you wish to (a)dd or (s)ubtract the numbers?"))
+
+if choice == "a":
+   print(num1+num2)
+elif choice == "s":
+   print (num1-num2)
